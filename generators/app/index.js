12990e1ec854f3387db365e520d678fbeef5ccef
Pascal Grimaud <pascalgrimaud@gmail.com>
2015-12-27 00:13:41 +0100

    Update banner

--- generators/app/index.js
+++ generators/app/index.js
@@ -27,27 +27,27 @@ module.exports = yeoman.generators.Base.extend({
     // },
     displayLogo: function () {
       console.log(' \n' +
-        chalk.red.bold('              Three::Modules\n') +
-        chalk.red.bold('          for:::the::Elven-Kings\n') +
-        chalk.red.bold('       under:the:sky,:Seven:for:the\n') +
-        chalk.red.bold('     Dwarf-Lords::in::their::halls:of\n') +
-        chalk.red.bold('    stone,:Nine             for:Mortal\n') +
-        chalk.red.bold('   :::Men:::     ________     doomed::to\n') +
-        chalk.red.bold(' die.:One   _,-\'...:... `-.    for:::the\n') +
-        chalk.red.bold(' ::Dark::  ,- .:::::::::::. `.   Lord::on\n') +
-        chalk.red.bold('his:dark ,\'  .:::::zzz:::::.  `.  :throne:\n') +
-        chalk.red.bold('In:::the/    ::::dMMMMMb::::    \ Land::of\n') +
-        chalk.red.bold(':Mordor:\    ::::dMMmgJP::::    / :where::\n') +
-        chalk.red.bold('::the::: \'.  \'::::YMMMP::::\'  ,\'  Shadows:\n') +
-        chalk.red.bold(' lie.::One  `. ``:::::::::\'\' ,\' Module::to\n') +
-        chalk.red.bold(' ::rule::    `-._```:\'\'\'_,-\'     ::them::\n') +
-        chalk.red.bold(' all,::One      `-----\'       Module::to\n') +
-        chalk.red.bold('   ::find:::                  them,:One\n') +
-        chalk.red.bold('    Module:::::to          bring::them\n') +
-        chalk.red.bold('      all::and::in:the:darkness:bind\n') +
-        chalk.red.bold('        them:In:the:Land:of:Mordor\n') +
-        chalk.red.bold('           where:::the::Shadows\n') +
-        chalk.red.bold('                :::lie.:::\n'));
+        chalk.red.bold('               Three::Modules\n') +
+        chalk.red.bold('           for:::the::Elven-Kings\n') +
+        chalk.red.bold('        under:the:sky,:Seven:for:the\n') +
+        chalk.red.bold('      Dwarf-Lords::in::their::halls:of\n') +
+        chalk.red.bold('     stone,:Nine             for:Mortal\n') +
+        chalk.red.bold('    :::Men:::     ________     doomed::to\n') +
+        chalk.red.bold('  die.:One   _,-\'...:... `-.    for:::the\n') +
+        chalk.red.bold('  ::Dark::  ,- .:::::::::::. `.   Lord::on\n') +
+        chalk.red.bold(' his:dark ,\'  .:::::zzz:::::.  `.  :throne:\n') +
+        chalk.red.bold(' In:::the/    ::::dMMMMMb::::    \ Land::of\n') +
+        chalk.red.bold(' :Mordor:\    ::::dMMmgJP::::    / :where::\n') +
+        chalk.red.bold(' ::the::: \'.  \'::::YMMMP::::\'  ,\'  Shadows:\n') +
+        chalk.red.bold('  lie.::One  `. ``:::::::::\'\' ,\' Module::to\n') +
+        chalk.red.bold('  ::rule::    `-._```:\'\'\'_,-\'     ::them::\n') +
+        chalk.red.bold('   all,::One     `-----\'       Module::to\n') +
+        chalk.red.bold('    ::find:::                  them,:One\n') +
+        chalk.red.bold('     Module:::::to          bring::them\n') +
+        chalk.red.bold('       all::and::in:the:darkness:bind\n') +
+        chalk.red.bold('         them:In:the:Land:of:Mordor\n') +
+        chalk.red.bold('            where:::the::Shadows\n') +
+        chalk.red.bold('                 :::lie.:::\n'));
 
       console.log(chalk.white.bold('         http://jhipster.github.io\n'));
       console.log(chalk.white('Welcome to the ' + chalk.bold('JHipster Module') + ' Generator! ' + chalk.yellow('v' + packagejs.version + '\n')));
